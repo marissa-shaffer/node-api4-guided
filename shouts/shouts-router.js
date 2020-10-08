e6085a8faf5e5f24eb8ebeed152054e133289646
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-10-07 20:20:23 -0500

    converted shouts router to use asyn await

--- shouts/shouts-router.js
+++ shouts/shouts-router.js
@@ -16,16 +16,24 @@ router.get("/:id", validateShoutId(), (req, res, next) => {
 	res.status(200).json(req.shout)
 })
 
-router.post("/", (req, res, next) => {
-	shoutsModel.add(req.body)
-		.then((data) => res.status(201).json(data))
-		.catch((err) => next(err))
+router.post("/", async (req, res, next) => {
+	try{
+		const data = await shoutsModel.add(req.body)
+		res.status(201).json(data)
+	} catch(err) {
+		next(err)
+	}
+	
 })
 
-router.delete("/:id", validateShoutId(), (req, res, next) => {
-	Shouts.remove(req.params.id)
-		.then(() => res.status(204).end())
-		.catch((err) => next(err))
+router.delete("/:id", validateShoutId(), async (req, res, next) => {
+	try { 
+		await Shouts.remove(req.params.id)
+		res.status(204).end()
+	} catch(err) {
+		next(err)
+	}
+		
 })
 
 function validateShoutId() {
